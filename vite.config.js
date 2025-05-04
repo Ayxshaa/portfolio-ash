c28f98d880b35dc7c404516d07622817c616a533
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-05-04 09:24:04 +0530

    jnn

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
-})
+  plugins: [
+    react(),tailwindcss(),
+    visualizer({ open: true }) // Optional: visualize chunk sizes
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning threshold if needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'three';
+            if (id.includes('postprocessing')) return 'postprocessing';
+            if (id.includes('framer-motion')) return 'framer';
+            if (id.includes('gsap')) return 'gsap';
+            if (id.includes('@react-three')) return 'react-three';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
