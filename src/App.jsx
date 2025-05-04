da684a82085a751efb07002a4f9a868656537817
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-05-04 19:02:57 +0530

    customFallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy } from "react";
+import CustomFallback from "./components/UI/CustomFallBack";
+
 const Loader = lazy(() => import("./components/UI/Loader"));
-const Layout = lazy(() => import("./Layout")); // Import the new Layout component
+const Layout = lazy(() => import("./Layout"));
 const MainContent = lazy(() => import("./MainContent"));
 const Gallery = lazy(() => import("./components/UI/Gallery"));
 
@@ -12,12 +14,12 @@ export default function App() {
   return (
     <>
       {isLoading ? (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<CustomFallback />}>
           <Loader onComplete={() => setIsLoading(false)} />
         </Suspense>
       ) : (
         <Router>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<CustomFallback />}>
             <Routes>
               <Route element={<Layout />}>
                 <Route path="/" element={<MainContent />} />
---
+++ src/components/UI/CustomFallBack.jsx
@@ -0,0 +1,14 @@
+// src/components/UI/CustomFallback.jsx
+import React from "react";
+ // You'll style it here
+
+const CustomFallback = () => {
+  return (
+    <div className="custom-fallback">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default CustomFallback;
+
