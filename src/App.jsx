9e9e62f802bbadfb84890513fdfb966e662e6565
Ayesha qureshi <qureshiayesha660@gmail.com>
2025-05-18 19:10:07 +0530

    lazywithPreload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy } from "react";
+import React, { useState, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { lazyWithPreload } from "./utils/lazywithPreload";
 import CustomFallback from "./components/UI/CustomFallBack";
 
-const Loader = lazy(() => import("./components/UI/Loader"));
-const Layout = lazy(() => import("./Layout"));
-const MainContent = lazy(() => import("./MainContent"));
-const Gallery = lazy(() => import("./components/UI/Gallery"));
+const Loader = React.lazy(() => import("./components/UI/Loader"));
+const Layout = React.lazy(() => import("./Layout"));
+const MainContent = React.lazy(() => import("./MainContent"));
+const Gallery = lazyWithPreload(() => import("./components/UI/Gallery"));
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,6 +20,16 @@ export default function App() {
       ) : (
         <Router>
           <Suspense fallback={<CustomFallback />}>
+            <nav >
+              <Link
+                to="/gallery"
+                onMouseEnter={() => {
+                  Gallery.preload(); // Preload Gallery on hover
+                }}
+              >
+              </Link> 
+            </nav>
+
             <Routes>
               <Route element={<Layout />}>
                 <Route path="/" element={<MainContent />} />
